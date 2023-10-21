d031bfe5fefc70b29f6e83a4955f2944860044d8
AbdulAzizMasood <masoodazizabdul@gmail.com>
2023-10-22 00:44:37 +0530

    added animation delay on navMenu Items

--- Ebook/script.js
+++ Ebook/script.js
@@ -1,13 +1,14 @@
 const navContainer = document.querySelector('.navItems');
-console.log(navContainer)
+// console.log(navContainer)
 const menuBtn = document.querySelector('#menuBtn');
 const closeBtn = document.querySelector('#closeBtn');
 const cartCon = document.querySelector('header .navItems a.shopping-cart i img')
-console.log(cartCon)
-console.log(menuBtn)
+// console.log(cartCon)
+// console.log(menuBtn)
 menuBtn.addEventListener('click',()=>{
+    navContainer.classList.add('active')
     navContainer.style.display = 'flex'
-    navContainer.style.right = '0%'
+    navContainer.style.right = '0';
     menuBtn.style.display = 'none';
     closeBtn.style.display = 'block'
     cartCon.src = 'Assets/Images/cart-icon-black-1.png';
@@ -15,10 +16,12 @@ menuBtn.addEventListener('click',()=>{
 
 
 closeBtn.addEventListener('click',()=>{
-    navContainer.style.right = '-100%'
-    navContainer.style.display = 'none';
-    menuBtn.style.display = 'block';
+    navContainer.classList.remove('active');
+    navContainer.style.right = '-100%';
+    // 👇 this was actually causing it to not animate back to '0'
+    // navContainer.style.display = 'none'; 
     closeBtn.style.display = 'none'
+    menuBtn.style.display = 'block';
     cartCon.src = 'Assets/Images/cart-svg.svg';
 
 })
